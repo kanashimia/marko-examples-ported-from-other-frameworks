c28d0309bebe78cfce50019a8def45e9aafb1e32
Mia Kanashi <chad@redpilled.dev>
2025-04-05 12:08:25 +0300

    Minor fixes.

--- vite.config.ts
+++ vite.config.ts
@@ -7,24 +7,7 @@ export default defineConfig({
   ],
   build: {
     target: "es2022",
-    minify: true,
     modulePreload: false,
-
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        // experimentalMinChunkSize: 99999999,
-        // Output ESM for the server build also.
-        // Remove when https://github.com/vitejs/vite/issues/2152 is resolved.
-        format: "es",
-        // intro: (chunk) => {
-        //     // console.log(chunk.fileName);
-        //     if (chunk.fileName === 'index.js') {
-        //         return `import "./style.css";`
-        //     }
-        //     return ''
-        // },
-      },
-    },
   },
 });
